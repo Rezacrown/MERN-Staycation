2aead0c211a670cea3921de8bc78d5ad18f0840c
Rezacrown <aryhidayatfebriana@gmail.com>
2023-10-22 08:23:54 +0800

    test for button component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -5,12 +5,53 @@ import Button from "./index";
 
 describe("Button components", () => {
   //
-  it("should have loading if has isLoading", ({ expect }) => {
+  it("should has defined", ({ expect }) => {
     const { getByText } = render(<Button>click</Button>);
 
-    const element = getByText('click');
+    const element = getByText("click");
 
     expect(element).toBeDefined();
     // expect(element).toBeInTheDocument();
   });
+
+  //
+  it("should have loading if has isLoading", ({ expect }) => {
+    const { getByText } = render(<Button isLoading>click</Button>);
+
+    const element = getByText(/...loading/i);
+
+    expect(element).toBeDefined();
+    // expect(element).toBeInTheDocument();
+  });
+  //
+  it("should have isDisabled", ({ expect }) => {
+    const { container } = render(
+      <Button isDisabled isPrimary>
+        click
+      </Button>
+    );
+
+    // const element = container.getAttribute('isDisabled')
+    // const element = container.getAttribute('isDisabled')
+    // const element = container.getAttribute('isDisabled')
+    // yg sukses
+    const element = container.getElementsByTagName("button");
+
+    expect(element[0]).toHaveProperty("disabled", true);
+  });
+
+  it("should have isPrimary", ({ expect }) => {
+    const { container } = render(
+      <Button isDisabled isPrimary>
+        click
+      </Button>
+    );
+    // yg sukses
+    const element = container.getElementsByClassName("btn-primary");
+
+    // debug(element);
+    expect(element[0]).toBeDefined();
+  });
+
+  //
 });
